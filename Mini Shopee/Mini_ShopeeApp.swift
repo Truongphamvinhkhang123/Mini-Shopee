34052ca1040d5f7fd703ca2fcbabaf01c0399604
Truongphamvinhkhang123 <131866195+Truongphamvinhkhang123@users.noreply.github.com>
2025-07-28 12:45:13 +0700

    Initial Commit

---
+++ Mini Shopee/Mini_ShopeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_ShopeeApp.swift
+//  Mini Shopee
+//
+//  Created by VINH KHANG on 28/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Mini_ShopeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mini ShopeeTests/Mini_ShopeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_ShopeeTests.swift
+//  Mini ShopeeTests
+//
+//  Created by VINH KHANG on 28/7/25.
+//
+
+import Testing
+@testable import Mini_Shopee
+
+struct Mini_ShopeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
