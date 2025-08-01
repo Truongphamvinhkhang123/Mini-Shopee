4921b7559c9c98994dd7cddb1b344d34192f9fae
Truongphamvinhkhang123 <131866195+Truongphamvinhkhang123@users.noreply.github.com>
2025-08-01 09:42:48 +0700

    update2

--- Mini Shopee/Mini_ShopeeApp.swift
+++ Mini Shopee/Mini_ShopeeApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Mini_ShopeeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+//                WelcomeView()
+            }
         }
     }
 }
